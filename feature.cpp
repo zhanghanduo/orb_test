0642bea16cbf1996fa0c2c69c38a9866a6ca7819
zhanghanduo <hzhang032@e.ntu.edu.sg>
2017-10-27 11:57:53 +0800

    detect whole image

--- feature.cpp
+++ feature.cpp
@@ -3,6 +3,81 @@
 //
 #include "feature.h"
 #include "lib.h"
+#include <Eigen/Dense>
+#include "Eigen/Core"
+#include "Eigen/Geometry"
 
-
-
+//namespace mvo{
+//
+//    struct Feature;
+//
+//    typedef std::list<Feature*> Features;//特征list
+//    typedef std::vector<cv::Mat> ImgPyr;//图像金字塔
+//
+//    /**	定义帧，保证帧的唯一性
+//     */
+//    class Frame : public Noncopyable
+//    {
+//    public:
+//        /**	帧的实例化，通过传入相机参数，获得的当前帧，及时间戳来确定
+//         */
+//        Frame(AbstractCamera* cam, const cv::Mat& img, double timestamp);
+//        ~Frame();
+//        /// 初始化新的图像帧，创建图像金字塔
+//        void initFrame(const cv::Mat& img);
+//
+//    private:
+//        /// 通过半采用的方式创建图像金字塔
+//        void createImgPyramid(const cv::Mat& img_level_0, int n_levels, ImgPyr& pyr);
+//
+//    public:
+//        static int                    frame_counter_;         //!< 创建帧的计数器，用于设置帧的唯一id
+//        int                           id_;                    //!< 帧的唯一id
+//        double                        timestamp_;             //!< 帧被记录的时间戳
+//        AbstractCamera                *cam_;                  //!< 相机模型
+//        ImgPyr                        img_pyr_;               //!< 图像金字塔
+//        Features                      fts_;                   //!< 图像中的特征List
+//    };
+//    typedef std::shared_ptr<Frame> FramePtr;
+//}
+//
+//
+//struct Feature
+//{
+//    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+//
+//    ///特征类型,目前先只考虑角点，后续考虑其它特征的时候，再进行添加
+//    enum FeatureType {
+//        CORNER//角点
+//    };
+//    FeatureType type;     //!< 特征类型，角点
+//    Frame* frame;         //!< 指针指向特征被检测到所对应的帧
+//    Eigen::Vector2d px;          //!< 特征在金字塔等级为0时的像素坐标
+//    int level;            //!< 特征被提取时，图像金字塔的等级
+//
+//    Feature(Frame* _frame, const Eigen::Vector2d& _px, int _level) :
+//            type(CORNER),
+//            frame(_frame),
+//            px(_px),
+//            level(_level)
+//    {}
+//
+//    ~Feature(){}
+//};
+//
+//AbstractDetector::AbstractDetector(
+//        const int img_width,
+//        const int img_height,
+//        const int cell_size,
+//        const int n_pyr_levels) :
+//        cell_size_(cell_size),
+//        n_pyr_levels_(n_pyr_levels),
+//        grid_n_cols_(ceil(static_cast<double>(img_width) / cell_size_)),
+//        grid_n_rows_(ceil(static_cast<double>(img_height) / cell_size_)),
+//        grid_occupancy_(grid_n_cols_*grid_n_rows_, false)
+//{}
+//
+//void AbstractDetector::resetGrid()
+//{
+//    std::fill(grid_occupancy_.begin(), grid_occupancy_.end(), false);
+//}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "lib.h"
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -14,9 +15,18 @@ int main ( int argc, char** argv )
         cout<<"usage: feature_extraction img1 img2"<<endl;
         return 1;
     }
+    int cellsize = 40;
     //-- Read Image
     Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
     Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
+    Mat img1_s,img2_s;
+    img1_s = img_1;
+    img2_s = img_2;
+//    cv::Size rzSize(1260, 870);
+//    cv::resize(img_1, img1_s, rzSize);
+//    cv::resize(img_2, img2_s, rzSize);
+
+
 
     //-- Initialization
     std::vector<KeyPoint> keypoints_1, keypoints_2;
@@ -27,23 +37,23 @@ int main ( int argc, char** argv )
     Ptr<DescriptorMatcher> matcher  = DescriptorMatcher::create ( "BruteForce-Hamming" );
 
     //-- 1. Detect corner position using Oriented FAST
-    detector->detect ( img_1,keypoints_1 );
-    detector->detect ( img_2,keypoints_2 );
+    detector->detect ( img1_s,keypoints_1 );
+    detector->detect ( img2_s,keypoints_2 );
 
     //-- 2. Calculate BRIEF descriptor according to the corner position
-    descriptor->compute ( img_1, keypoints_1, descriptors_1 );
-    descriptor->compute ( img_2, keypoints_2, descriptors_2 );
+    descriptor->compute ( img1_s, keypoints_1, descriptors_1 );
+    descriptor->compute ( img2_s, keypoints_2, descriptors_2 );
 
     Mat outimg1;
-    drawKeypoints( img_1, keypoints_1, outimg1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    drawKeypoints( img1_s, keypoints_1, outimg1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
     imshow("ORB feature",outimg1);
 
-    //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
+    //-- 3. Use Hamming Distance to match BRIEF descriptor
     vector<DMatch> matches;
     //BFMatcher matcher ( NORM_HAMMING );
     matcher->match ( descriptors_1, descriptors_2, matches );
 
-    //-- 第四步:匹配点对筛选
+    //-- 4. Filter matched points
     double min_dist=10000, max_dist=0;
 
     //找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
@@ -66,14 +76,15 @@ int main ( int argc, char** argv )
             good_matches.push_back ( matches[i] );
         }
     }
+    std::cout << "Matches: " << matches.size() << " and Good matches: " << good_matches.size() << std::endl;
 
     //-- 第五步:绘制匹配结果
     Mat img_match;
     Mat img_goodmatch;
-    drawMatches ( img_1, keypoints_1, img_2, keypoints_2, matches, img_match );
-    drawMatches ( img_1, keypoints_1, img_2, keypoints_2, good_matches, img_goodmatch );
-    imshow ( "所有匹配点对", img_match );
-    imshow ( "优化后匹配点对", img_goodmatch );
+    drawMatches ( img1_s, keypoints_1, img2_s, keypoints_2, matches, img_match );
+    drawMatches ( img1_s, keypoints_1, img2_s, keypoints_2, good_matches, img_goodmatch );
+    imshow ( "all matches", img_match );
+    imshow ( "matches after optimization", img_goodmatch );
     waitKey(0);
 
     return 0;
